bbe6acf5d7c824a439ed5b0c06a52a295209e8a1
Lance <lance.purple@gmail.com>
2015-08-23 19:45:07 -0500

    Refactor CashboxChartSourceTests

--- src/misc/top40/chart/tests/CashboxChartSourceTests.java
+++ src/misc/top40/chart/tests/CashboxChartSourceTests.java
@@ -7,12 +7,17 @@ import misc.top40.chart.ChartSources;
 
 import org.junit.Test;
 
-public class ChartSourceTests {
+public class CashboxChartSourceTests {
 
 	@Test
-	public void testCashboxChartSourceDates() {
+	public void testEarliestDateForCashboxChartSource() {
 		ChartSource cashbox = ChartSources.CASHBOX.get();
 		assertEquals(cashbox.earliestDate(), ChartDate.of(1950,1,7));
+	}
+
+	@Test
+	public void testLatestDateForCashboxChartSource() {
+		ChartSource cashbox = ChartSources.CASHBOX.get();
 		assertEquals(cashbox.latestDate(),  ChartDate.of(1996,11,16));
 	}
 
